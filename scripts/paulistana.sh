ebae19eef9650160a2a2b41b86237d3e26d72730
Luis Malta <luis.malta@kmee.com.br>
2020-10-22 10:20:43 -0300

    [FIX] Paulistana schema version

--- scripts/paulistana.sh
+++ scripts/paulistana.sh
@@ -1,4 +1,4 @@
-erpbrasil-edoc-gen-download-schema -n paulistana -v v02 -u http://notadomilhao.prefeitura.sp.gov.br/arquivos/schemas.zip/@@download/file/schemas.zip
 erpbrasil-edoc-gen-generate-python -m nfse -n paulistana -v v02 -i "PedidoEnvioLoteRPS_v01|PedidoConsultaLote_v01|RetornoEnvioLoteRPS_v01|RetornoConsulta_v01|RetornoInformacoesLote_v01|PedidoConsultaNFe_v01" -d .
+erpbrasil-edoc-gen-download-schema -n paulistana -v v02 -u http://notadomilhao.prefeitura.sp.gov.br/arquivos/schemasv02.zip/@@download/file/schemasv02.zip
 # O servico_enviar_lote_rps_envio_v03 foi necessário pois a classe EnviarLoteRpsEnvio
 # não estava no arquivo de retorno.
